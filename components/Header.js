5e912606d393cb8b49dc9b9ca5823e977c6ad4bf
Abhishek jha <abhishek@talquo.com>
2023-02-21 23:46:56 +0545

    TD-1: Added header

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,46 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useContext} from 'react';
+import {View, Text, Image, SafeAreaView} from 'react-native';
+import ColorThemeContext from '../app/context/ColorThemeContext';
+import ColorTheme from './ColorTheme';
 
-const Header = () => {
+const Header = ({profileImage, name}) => {
+  const {isDarkEnabled} = useContext(ColorThemeContext);
   return (
-    <View>
-      <Text>This is Header</Text>
-    </View>
+    <>
+      <SafeAreaView>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            display: 'flex',
+            padding: 10,
+          }}>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <View>
+              <Image
+                source={{uri: profileImage}}
+                style={{height: 30, width: 30, borderRadius: 25}}
+              />
+            </View>
+            <Text
+              style={{
+                marginLeft: 5,
+                color: isDarkEnabled ? 'darkgrey' : '#000',
+              }}>
+              Welcome, {name}!
+            </Text>
+          </View>
+          <ColorTheme />
+        </View>
+      </SafeAreaView>
+      <View style={{height: 1, width: '100%', backgroundColor: 'grey'}}></View>
+    </>
   );
 };
 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,5 @@
-import {View, Text} from 'react-native';
-import React, {useContext, useState} from 'react';
-import ColorTheme from '../components/ColorTheme';
+import {View} from 'react-native';
+import React, {useContext} from 'react';
 import ColorThemeContext from '../app/context/ColorThemeContext';
 import FeedScreen from './FeedScreen';
 
@@ -8,13 +7,7 @@ const HomeScreen = () => {
   const {isDarkEnabled} = useContext(ColorThemeContext);
   return (
     <View
-      style={{
-        backgroundColor: isDarkEnabled ? '#121212' : '#fff',
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <ColorTheme />
+      style={{flex: 1, backgroundColor: isDarkEnabled ? '#121212' : '#fff'}}>
       <FeedScreen />
     </View>
   );
