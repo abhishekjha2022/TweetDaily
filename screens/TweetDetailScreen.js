e58f3898f53cb804bff67c603a671b767ec4edc8
Abhishek jha <abhishek@talquo.com>
2023-02-24 00:24:18 +0545

    Passed all props to tweet detailed screen

--- screens/TweetDetailScreen.js
+++ screens/TweetDetailScreen.js
@@ -1,18 +1,27 @@
 import {useNavigation, useRoute} from '@react-navigation/native';
 import React, {useContext} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
 import ColorThemeContext from '../app/context/ColorThemeContext';
 
 const TweetDetailScreen = () => {
   const {params} = useRoute();
-  const {content} = params;
+  const {item, profileImage, userName, name} = params;
   const {isDarkEnabled} = useContext(ColorThemeContext);
   return (
     <View
       style={{backgroundColor: isDarkEnabled ? '#121212' : '#fff', flex: 1}}>
+      <Image source={{uri: profileImage}} style={{height: 50, width: 50}} />
       <Text style={{color: isDarkEnabled ? 'darkgrey' : '#000'}}>
-        {content}
+        {userName}
       </Text>
+      <Text>{name}</Text>
+      <Text>{item.content}</Text>
+      {item.uploadedImage && (
+        <Image
+          source={{uri: item.uploadedImage}}
+          style={{height: 50, width: 50}}
+        />
+      )}
     </View>
   );
 };
