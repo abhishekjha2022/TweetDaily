0ea3f7879b7af0a941bf766b3d7ed136c26386cd
Abhishek jha <abhishek@talquo.com>
2023-02-25 15:59:02 +0545

    Added tob tabs navigator

---
+++ screens/MessageScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const MessageScreen = () => {
+  return (
+    <View>
+      <Text>This is message screen</Text>
+    </View>
+  );
+};
+export default MessageScreen;
